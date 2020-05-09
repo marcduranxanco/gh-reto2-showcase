afa862f6605d841591e8524164c6ea41ebebb6fd
MarcDuran <marcduranxanco@hotmail.com>
2020-05-09 22:17:17 +0200

    UPDATE: Drag and drop working

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const games = document.querySelector('#games');
 const shoppingCart = document.querySelector('.shopping-cart');
 const draggablesDiv = document.querySelectorAll('.draggable-div');
 
+
 const dragStartf = () => {
     focusSC(true);
 }
@@ -17,26 +18,42 @@ const dragEndf = () => {
 movies.addEventListener("click", () => { console.log("Moovies"); });
 tvshows.addEventListener("click", () => { console.log("tvshows"); });
 games.addEventListener("click", () => { console.log("games"); });
-shoppingCart.addEventListener("click", () => { console.log("shoping c"); });
-draggablesDiv.forEach( (dragableDiv, currentIndex, listObj) => { 
+
+shoppingCart.addEventListener("click", () => {
+    console.log("shoping c");
+});
+
+shoppingCart.addEventListener("dragover", ev => {
+    dragOver(ev);
+});
+
+draggablesDiv.forEach( (dragableDiv) => {
+    //Drag starts. When begin the dragg
         dragableDiv.addEventListener("dragstart", dragStartf);
+    //Drag end.
         dragableDiv.addEventListener("dragend", dragEndf);
-        
     }
 );
+
 
 const focusSC = (isFocus) => {
     let allExcepSc = document.querySelectorAll('nav, header, .draggable-div, .menu-categories');
     if (isFocus) {
-        allExcepSc.forEach( (currentValue, currentIndex, listObj) => { 
+        allExcepSc.forEach( (currentValue) => { 
             currentValue.classList.add('opac');
             shoppingCart.classList.add('hovered-shopping-cart');
         });
     } else {
-        allExcepSc.forEach( (currentValue, currentIndex, listObj) => { 
+        allExcepSc.forEach( (currentValue) => { 
             currentValue.classList.remove('opac');
             shoppingCart.classList.remove('hovered-shopping-cart');
         });
         
     }
 }
+
+const dragOver = (ev) => {
+    ev.preventDefault();
+    console.log('over');
+}
+
